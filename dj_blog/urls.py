bbfdfafc49f3b6224412620819adf565ce20bb09
nouh ben kadi <benkadi.nouh@icloud.com>
2020-07-09 20:40:52 +0100

    Added reset,done,confirm routes using generic views

--- dj_blog/urls.py
+++ dj_blog/urls.py
@@ -17,6 +17,20 @@ urlpatterns = [
     #path('login/', user_views.MyLoginView.as_view(template_name='users/login.html'), name='login'),
     #path('login/', user_views.MyLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    #Password reset via email
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+    # when the email for changing the password is successfuly sent
+    path('password-reset/done/', 
+          auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'
+          ),
+    # When we click to send ythe email to reset the password django will 
+    # look into this view and expects
+    # to find with url: the user id coded in base 64 and a token to tell if the password is correct
+    path('password-reset-confirm/<uidb64>/<token>/', 
+          auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+          name='password_reset_confirm'
+          ),
+
     #profiles
     path('profile/', user_views.profile, name='profile'),
     path('admin/', admin.site.urls),
